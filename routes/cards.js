8ed961942e8b68d814ff407db569f383cfd4383d
Roee Shapira <roy.shap1@gmail.com>
2018-02-11 12:34:46 +0200

    Gives a randon card

--- routes/cards.js
+++ routes/cards.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const {data} = require('../data/flashcardData.json');
 const {cards} = data;
 
+router.get('/', (req, res) => {
+	const id = Math.floor(Math.random() * cards.length);
+	const templateData = {id , text: cards[id].question, hint: cards[id].hint, sideToShow: 'answer', sideToShowDisplay: 'Answer'};
+	res.render(`card`, templateData);
+});
+
 router.get('/:id', (req, res) => {
 	const {side} = req.query;
 	const {id} = req.params;
